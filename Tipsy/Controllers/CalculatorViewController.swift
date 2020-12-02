e010f8411e919a4668d1900148fac03cfa356d89
Rob Peschke <rspesc31@gmail.com>
2020-12-02 12:11:58 -0500

    app is up and running

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    var eachPayAmount: Double = 0.0
+    var tipPercentage:Double = 0.0
+    var splitCount: Double = 0.0
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
@@ -18,6 +22,8 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func tipChanged(_ sender: UIButton) {
         let buttonTitlePressed = sender.title(for: .normal) ?? ""
+        
+        billTextField.endEditing(true)
         
         // there has to be a better way...
         if buttonTitlePressed == "20%" {
@@ -37,9 +43,35 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
     }
     
     @IBAction func calculatePressed(_ sender: Any) {
+        
+        let totalBillAmount = Double(billTextField.text!)
+        splitCount = Double(splitNumberLabel.text!)!
+
+        if zeroPctButton.isSelected == true {
+            tipPercentage = 0.0
+        } else if tenPctButton.isSelected == true {
+            tipPercentage = 0.1
+        } else if twentyPctButton.isSelected == true {
+            tipPercentage = 0.2
+        } else {
+            print("No button selected!")
+        }
+        
+        eachPayAmount = ((totalBillAmount! * tipPercentage) + totalBillAmount! ) / splitCount
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.paymentPerPerson = String(format: "%.2f", eachPayAmount)
+            destinationVC.tipPercentage = tipPercentage
+            destinationVC.splitCount = splitCount
+        }
     }
     
 }
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
+    
+    var paymentPerPerson: String?
+    var tipPercentage:Double = 0.0
+    var splitCount: Double = 0.0
 
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("here is the result on the second screen", paymentPerPerson!)
+        totalLabel.text = paymentPerPerson!
+        settingsLabel.text = "Split between \(Int(splitCount)) people, with \(Int(tipPercentage*100))% tip."
     }
     
 
