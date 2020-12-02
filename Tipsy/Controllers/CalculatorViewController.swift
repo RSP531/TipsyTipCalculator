0baaa588b2f32988b8a51a97dd3ad3b19904b418
Rob Peschke <rspesc31@gmail.com>
2020-12-02 12:18:10 -0500

    finalized and revised code

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
-    var eachPayAmount: Double = 0.0
-    var tipPercentage:Double = 0.0
-    var splitCount: Double = 0.0
-    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var eachPayAmount: Double = 0.0
+    var tipPercentage:Double = 0.0
+    var splitCount: Double = 0.0
+    var totalBillAmount: Double = 0.0
     
     @IBAction func tipChanged(_ sender: UIButton) {
         let buttonTitlePressed = sender.title(for: .normal) ?? ""
@@ -48,7 +49,7 @@ class CalculatorViewController: UIViewController {
     
     @IBAction func calculatePressed(_ sender: Any) {
         
-        let totalBillAmount = Double(billTextField.text!)
+        totalBillAmount = Double(billTextField.text!)!
         splitCount = Double(splitNumberLabel.text!)!
 
         if zeroPctButton.isSelected == true {
@@ -61,16 +62,26 @@ class CalculatorViewController: UIViewController {
             print("No button selected!")
         }
         
-        eachPayAmount = ((totalBillAmount! * tipPercentage) + totalBillAmount! ) / splitCount
+        eachPayAmount = ((totalBillAmount * tipPercentage) + totalBillAmount ) / splitCount
+        
+        //In Main.storyboard there is a segue between CalculatorVC and ResultsVC with the identifier "goToResults".
+        //This line triggers the segue to happen.
         self.performSegue(withIdentifier: "goToResult", sender: self)
     }
     
+    //This method gets triggered just before the segue starts.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //If the currently triggered segue is the "goToResults" segue.
         if segue.identifier == "goToResult" {
+            
+            //Get hold of the instance of the destination VC and type cast it to a ResultViewController.
             let destinationVC = segue.destination as! ResultsViewController
+            
+            //Set the destination ResultsViewController's properties.
             destinationVC.paymentPerPerson = String(format: "%.2f", eachPayAmount)
             destinationVC.tipPercentage = tipPercentage
             destinationVC.splitCount = splitCount
+            
         }
     }
     
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -17,28 +17,14 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
-    @IBAction func recalculatePressed(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        print("here is the result on the second screen", paymentPerPerson!)
         totalLabel.text = paymentPerPerson!
         settingsLabel.text = "Split between \(Int(splitCount)) people, with \(Int(tipPercentage*100))% tip."
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
